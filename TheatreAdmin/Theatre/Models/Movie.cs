1aae142771fe18d905a0abeab143502b1f470936
Angie Hollingworth <48933873+Bimsickle@users.noreply.github.com>
2020-08-01 17:32:07 +1000

    with any luck, a model was sucessfully created

--- TheatreAdmin/Theatre/Models/Movie.cs
+++ TheatreAdmin/Theatre/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,30 @@ namespace Theatre.Models
 {
     public class Movie
     {
+        public int Id { get; set; }
+        public string name { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Director { get; set; }
+        public int MyProperty { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public EmailAddressAttribute EmailAddress { get; set; }
+        public enum Language 
+        {
+            English,
+            Japanese,
+            Chinese
+        }
+        public int CategoryId { get; set; }
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+
+        public virtual ICollection<Movie> Movies { get; set; }
     }
 }
+
