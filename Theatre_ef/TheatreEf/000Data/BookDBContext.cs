baf58aee936740b293fe854ec8c777429ef6b9e2
Angie Hollingworth <48933873+Bimsickle@users.noreply.github.com>
2020-08-04 13:17:26 +1000

    Pushing whatever changes were made to the book app last night

---
+++ Theatre_ef/TheatreEf/000Data/BookDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Threading.Tasks;
+
+namespace TheatreEf._000Data
+{
+    public class BookDBContext : DbContext
+    {
+        public BookDBContext(DbContextOptions<BookDBContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
