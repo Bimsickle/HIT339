9893fccf0d9b641a22278deb578177a2fdbd0618
Angie Hollingworth <48933873+Bimsickle@users.noreply.github.com>
2020-08-04 19:05:41 +1000

    deleted first book app, trying again

---
+++ Books/BookShop/000Data/EFModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace BookShop._000Data
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        [StringLength(50)]
+        [Required]
+        public string Title { get; set; }
+        public Author Author { get; set; }
+    }
+
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        [StringLength(50)]
+        [Required]
+        public string AuthorName { get; set; }
+        public ICollection<Book> Book { get; set; }
+    }
+}
+
